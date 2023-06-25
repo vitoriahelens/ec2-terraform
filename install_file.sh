959b03b0db20491c615f2cef2f1305742d8461df
vitoriahelens <vitoria.devops@outlook.com>
2023-06-25 20:27:18 -0300

    instalando docker e imagens

--- install_file.sh
+++ install_file.sh
@@ -1,7 +1,15 @@
-#! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo ufw allow 80
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+#!/bin/bash
+sudo apt update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt update
+sudo apt-get install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run -d --name apache-server -p 80:80 -v apache-data:/usr/local/apache2/htdocs httpd
+sleep 60
+docker run -p 5432:5432 -v /tmp/database:/var/lib/postgresql/data -e POSTGRES_PASSWORD=devops123 postgres
