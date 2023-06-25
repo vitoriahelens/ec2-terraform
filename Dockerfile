7969d8c3c8184c815072fbdb43c5aca18fcf731b
vitoriahelens <vitoria.devops@outlook.com>
2023-06-25 01:46:29 -0300

    configurando arquivos docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM php:latest
 WORKDIR /var/www/html
-COPY hello-world.php /var/www/html
+COPY hello-world.php ./var/www/html
 EXPOSE 80
 CMD ["php", "-S", "0.0.0.0:80"]
--- docker.tf
+++ docker.tf
@@ -1,13 +1,30 @@
-/*resource "docker_image" "php_app" {
-  name = "my-php-app"
-}*/
+resource "docker_image" "devops-php-image" {
+  name = "devops-php-image"
+}
 
-/*resource "docker_container" "php_container" {
-  name  = "my-php-container"
-  image = docker_image.php_app.latest
+resource "docker_container" "php_container" {
+  name  = "devops-php-container"
+  image = docker_image.devops-php-image.latest
 
   ports {
     internal = 80
     external = 8080
   }
-}*/
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install docker -y",
+      "sudo service docker start",
+      "sudo usermod -aG docker ubuntu",
+      "sudo docker run -d -p 80:80 devops-php-image"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("./devops")
+      host        = aws_subnet.public_subnet.id
+    }
+  }
+}
